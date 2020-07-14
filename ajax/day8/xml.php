57a077fc0f6e20880d2c65beaa66e9de232b3f12
yxDeveloper <dilutian0415@gmail.com>
2020-07-14 22:49:31 +0800

    ajaxpractice

---
+++ ajax/day8/xml.php
@@ -0,0 +1,13 @@
+<?php 
+ 
+ header('Content-Type: application/xml');
+
+ ?>
+<?xml version="1.1" encoding="utf-8"?>
+ <person>
+	 <name>石敢当</name>
+	 <age>19</age>
+	 <gender>男</gender>
+ </person>
+ 
+ 
