98209ce9d0d69876d709957eb285823d4b0be79b
yxDeveloper <dilutian0415@gmail.com>
2020-07-14 22:49:44 +0800

    ajaxpractice

---
+++ ajax/day9/callback.js
@@ -0,0 +1,33 @@
+// console.log(1)
+// ajax('time.php',function(res) {
+// 	console.log(3)
+// 	ajax('time.php',function(res){
+// 		ajax('time.php', function (res) {
+// 		      ajax('time.php', function (res) {
+// 		        ajax('time.php', function (res) {
+// 		          ajax('time.php', function (res) {
+// 		
+// 		          })
+// 		        })
+// 		      })
+// 		    })
+// 	})
+// })
+// 
+// ajax('time.php')
+// 	.then(function(res) {
+// 		return ajax('time.php')
+// 	})
+// 	.then(function(res) {
+// 		return ajax('time.php')
+// 	})
+// 	.then(function(res) {
+// 		return ajax('time.php')
+// 	})
+// 	.then(function(res) {
+// 		return ajax('time.php')
+// 	})
+// 	
+// function load () {
+//   ajax('time.php', load)
+// }
---
+++ ajax/day9/cors.php
@@ -0,0 +1,30 @@
+<?php 
+
+$conn = mysqli_connect('localhost','root','123456','test');
+
+$query = mysqli_query($conn,'select * from users');
+
+while ($row = mysqli_fetch_assoc($query)) {
+	$data[] = $row;
+}
+
+// 一行代码搞定
+//允许跨域请求
+
+header('Access-Control-Allow-Origin: *');
+
+header('Content-Type: application/json');
+echo json_encode($data);
+
+
+
+
+
+
+
+
+
+
+
+
+
